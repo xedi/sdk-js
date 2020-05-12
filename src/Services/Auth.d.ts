73ac35e434aab67ce1e6319d242fffa5eec35c0c
Chris Smith <chris@xedi.com>
2020-05-12 13:44:00 +0100

    Updated the Auth definition to include a AuthResponse object

--- src/Services/Auth.d.ts
+++ src/Services/Auth.d.ts
@@ -2,10 +2,17 @@ import Service from './Service';
 import { User, Business } from '../Models/Models';
 import Xuid, { SupportedXuid } from '../Utils/Xuid';
 
+declare interface AuthResponse extends Object
+{
+    tokens: Object;
+    user: Object;
+    business?: Object;
+}
+
 declare class Auth extends Service {
-    logout(): PromiseConstructor;
-    login(email: String, password: String): PromiseConstructor;
-    switchContexts(business_uuid: Xuid<SupportedXuid.Business>): PromiseConstructor;
+    logout(): Promise<Boolean>;
+    login(email: String, password: String): Promise<AuthResponse>;
+    switchContexts(business_uuid: Xuid<SupportedXuid.Business>): Promise<AuthResponse>;
     user?: User;
     business?: Business;
 }
