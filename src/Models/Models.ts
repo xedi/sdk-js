b7c3e2dec53556fb1cd7bd4954d97a87cab61791
Chris Smith <chris@xedi.com>
2020-05-26 14:16:43 +0100

    [feat/products] Added the products model and service based on PDK-JS

--- src/Models/Models.ts
+++ src/Models/Models.ts
@@ -1,12 +1,14 @@
 import User from './User';
 import Business from './Business';
+import Product from './Product';
 
-type Models = User | Business;
+type Models = User | Business | Product;
 interface Collection<T = Models> extends Array<T | Models> {}
 
 export {
     Models,
     Collection,
     User,
-    Business
+    Business,
+    Product,
 };
--- src/Services/Services.ts
+++ src/Services/Services.ts
@@ -1,9 +1,11 @@
 import Auth from "./Auth";
 import Businesses from "./Businesses";
+import Products from "./Products";
 import Users from "./Users";
 
 export {
     Auth,
     Businesses,
+    Products,
     Users,
 };
