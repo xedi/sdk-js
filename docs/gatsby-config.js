d8950169a0abb4edaa2b599c6f79fd74976fb0ab
Chris Smith <chris@xedi.com>
2020-06-04 13:41:25 +0100

    Documentation: Now a PWA and installable in Chrome and android

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -18,6 +18,18 @@ module.exports = {
                 repoRootPath: '..',
                 icon: './src/images/xedi-logo-dark.png'
             }
-        }
+        },
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+              name: 'XEDI JavaScript SDK',
+              short_name: 'XEDI JS SDK',
+              start_url: '/',
+              background_color: '#24292e',
+              theme_color: '#24292e',
+              display: 'standalone'
+            },
+        },
+        'gatsby-plugin-offline'
     ],
 }
