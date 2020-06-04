6b2faae9f52b59c1522f08165f1b61d1b2922478
Chris Smith <chris@xedi.com>
2020-06-04 13:30:38 +0100

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-81f589c37cc87b45459a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{801:function(e,t,n){"use strict";var u=n(53);t.__esModule=!0,t.default=void 0;var o=u(n(151)),r=u(n(0)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-81f589c37cc87b45459a.js.map
