644ef207d953772530a127cd9555e298db439556
Callum Eddisford <me@callumeddisford.co.uk>
2020-06-05 14:04:03 +0100

    Missed this file

--- src/Models/TaxRate.ts
+++ src/Models/TaxRate.ts
@@ -4,9 +4,9 @@ import Xuid, { SupportedXuid } from "../Utils/Xuid";
  * TaxRate
  */
 export default interface TaxRate extends Object {
-    _id: Xuid<SupportedXuid.Price>;
-    product_id: Xuid<SupportedXuid.Product>;
-    partnership_id: Xuid<SupportedXuid.Partnership>;
-    tax_rate_id: Xuid<SupportedXuid.TaxRate>;
-    cost: number;
+    _id: Xuid<SupportedXuid.TaxRate>;
+    business_id: Xuid<SupportedXuid.Business>;
+    description: string,
+    percentage: number,
+    code: string
 }
