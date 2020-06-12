cc7a80919a10051bffd46bb69a3050ca0bbb6d08
Chris Smith <chris@xedi.com>
2020-06-12 14:23:44 +0100

    Fixed sniff violations

--- src/Logger/NullLogger.ts
+++ src/Logger/NullLogger.ts
@@ -4,22 +4,27 @@ class NullLogger implements LoggerInterface
 {
     log(message?: any, ...optionalParams: any[]): void
     {
+        // noop
     }
 
     info(message?: any, ...optionalParams: any[]): void
     {
+        // noop
     }
 
     debug(message?: any, ...optionalParams: any[]): void
     {
+        // noop
     }
 
     error(message?: any, ...optionalParams: any[]): void
     {
+        // noop
     }
 
     warn(message?: any, ...optionalParams: any[]): void
     {
+        // noop
     }
 }
 
