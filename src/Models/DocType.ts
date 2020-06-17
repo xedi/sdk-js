aee8d6046584b334cc03d69bd72db36bab86a03a
Callum Eddisford <me@callumeddisford.co.uk>
2020-06-17 11:24:52 +0100

    Improved model

--- src/Models/DocType.ts
+++ src/Models/DocType.ts
@@ -1,4 +1,4 @@
-import Xuid, { SupportedXuid } from '../Utils/Xuid';
+import Xuid, {SupportedXuid} from '../Utils/Xuid';
 
 /**
  * DocType
@@ -6,5 +6,9 @@ import Xuid, { SupportedXuid } from '../Utils/Xuid';
 export default interface DocType extends Object {
     _id: Xuid<SupportedXuid.DocType>;
     type: string;
-    message_types: object;
+    message_types: {
+        tradacoms?: string[];
+        edifact?: string[];
+        x12?: string[];
+    };
 }
