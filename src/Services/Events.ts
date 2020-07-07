bfabc339aff9195425d9054f43771015a90825b7
Chris Smith <chris@xedi.com>
2020-07-07 12:51:18 +0100

    Added the contextual indexes

--- src/Services/Events.ts
+++ src/Services/Events.ts
@@ -3,22 +3,89 @@ import Service from './Service';
 import Xuid, { SupportedXuid } from '../Utils/Xuid';
 import JsonResponse from '../Interfaces/JsonResponse';
 import { Collection, Event } from '../Models/Models';
+import PaginatedJsonResponse from '../Interfaces/PaginatedJsonResponse';
 
+/**
+ * Events API
+ */
 class Events extends Service
 {
-    list() {
+    /**
+     * Lists all events
+     *
+     * @param [pageNumber]
+     *
+     * @returns Promise<PaginatedJsonResponse<Event[]>>
+     */
+    list(pageNumber: number = 1) {
         return this.client
-            .get<JsonResponse<Collection<Event>>>('1/events')
-            .then((response: AxiosResponse<JsonResponse<Collection<Event>>>) => {
-                return response.data.data;
+            .get<PaginatedJsonResponse<Event>>('1/events', { params: { page: pageNumber } })
+            .then((response: AxiosResponse<PaginatedJsonResponse<Event>>) => {
+                return response.data;
             });
     }
 
+    /**
+     * Get an event by ID
+     *
+     * @param eventId
+     *
+     * @returns Event
+     */
     get(eventId: Xuid<SupportedXuid.Event>) {
         return this.client
             .get<JsonResponse<Event>>(`1/events/${eventId}`)
             .then((response: AxiosResponse<JsonResponse<Event>>) => {
                 return response.data.data;
+            });
+    }
+
+    /**
+     * Get Events for specific user
+     *
+     * @param userId
+     * @param [pageNumber]
+     *
+     * @returns Promise<PaginatedJsonResponse<Event[]>>
+     */
+    getByUser(userId: Xuid<SupportedXuid.Event>, pageNumber: number = 1) {
+        return this.client
+            .get<PaginatedJsonResponse<Event>>(`1/users/${userId}/events`, { params: { page: pageNumber } })
+            .then((response: AxiosResponse<PaginatedJsonResponse<Event>>) => {
+                return response.data;
+            });
+    }
+
+    /**
+     * Get events for a specific business
+     *
+     * @param businessId
+     * @param [pageNumber]
+     *
+     * @returns Promise<PaginatedJsonResponse<Event[]>>
+     */
+    getByBusiness(businessId: Xuid<SupportedXuid.Business>, pageNumber: number = 1) {
+        return this.client
+            .get<PaginatedJsonResponse<Event>>(`1/businesses/${businessId}/events`, { params: { page: pageNumber } })
+            .then((response: AxiosResponse<PaginatedJsonResponse<Event>>) => {
+                return response.data;
+            });
+    }
+
+    /**
+     * Gets Events by user for specific business
+     *
+     * @param businessId
+     * @param userId
+     * @param [pageNumber]
+     *
+     * @returns Promise<PaginatedJsonResponse<Event[]>>
+     */
+    getByUserForBusiness(businessId: Xuid<SupportedXuid.Business>, userId: Xuid<SupportedXuid.User>, pageNumber: number = 1) {
+        return this.client
+            .get<PaginatedJsonResponse<Event>>(`1/businesses/${businessId}/users/${userId}/events`, { params: { page: pageNumber } })
+            .then((response: AxiosResponse<PaginatedJsonResponse<Event>>) => {
+                return response.data;
             });
     }
 }
