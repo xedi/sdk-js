32107e6651487240ff01c94e50654d0a2171e008
Adam Witeszczak <adam@xedi.com>
2020-09-24 10:13:39 +0100

    [XN-242, XN-243] Added sdk endpoint and docs for listing mailing lists for a business

--- src/Services/MailingLists.ts
+++ src/Services/MailingLists.ts
@@ -1,6 +1,6 @@
 import Service from './Service';
 import {AxiosResponse} from 'axios';
-import {MailingList} from '../Models/Models';
+import {Collection, MailingList} from '../Models/Models';
 import JsonResponse from '../Interfaces/JsonResponse';
 import Xuid, {SupportedXuid} from "../Utils/Xuid";
 import PaginatedJsonResponse from "../Interfaces/PaginatedJsonResponse";
@@ -17,6 +17,19 @@ class MailingLists extends Service {
             .post<JsonResponse<MailingList>>(`1/mailinglists`, mailinglist)
             .then((response: AxiosResponse<JsonResponse<MailingList>>) => {
                 return response.data.data
+            });
+    }
+
+    /**
+     * List all mailinglists for a business
+     * @param business_uuid
+     * @returns Promise<Instance>
+     */
+    list(business_uuid: Xuid<SupportedXuid.Business>) {
+        return this.client
+            .get<JsonResponse<Collection<MailingList>>>('1/businesses/' + business_uuid + '/mailinglists')
+            .then((resp: AxiosResponse<JsonResponse<Collection<MailingList>>>) => {
+                return resp.data.data;
             });
     }
 }
