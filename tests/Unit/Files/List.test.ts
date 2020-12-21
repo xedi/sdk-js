ca40cea2e6ab321f67b8b04441052b1dff79415d
Michael Middleton <midmiddleton@googlemail.com>
2020-12-21 13:39:18 +0000

    Fixes tests

--- tests/Unit/Files/List.test.ts
+++ tests/Unit/Files/List.test.ts
@@ -42,15 +42,6 @@ describe('Files@list', () => {
             ]
         };
 
-        nock('http://api-gateway.localhost')
-            .get('/1/files')
-            .reply(200, positiveResponse);
-
-        const mockConfig = new Config();
-        const axios: AxiosInstance = Axios.create({
-            baseURL: 'http://api-gateway.localhost'
-        });
-
         const params: ListParams = {
             page: 1,
             type: 'test',
@@ -59,6 +50,24 @@ describe('Files@list', () => {
             network_connection_id: 'test-id',
             network_configuration_id: 'test-id',
         }
+
+        nock('http://api-gateway.localhost')
+            .get('/1/files')
+            .query({
+                page: 1,
+                type: 'test',
+                business_id: 'business-id',
+                user_id: 'user-id',
+                network_connection_id: 'test-id',
+                network_configuration_id: 'test-id',
+            })
+            .reply(200, positiveResponse);
+
+        const mockConfig = new Config();
+        const axios: AxiosInstance = Axios.create({
+            baseURL: 'http://api-gateway.localhost'
+        });
+
 
         const filesService = new Files(mockConfig, axios);
         const response = await filesService.list(params);
