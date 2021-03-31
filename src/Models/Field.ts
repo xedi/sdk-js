7942678bd4718228a18ac35b42bc0f83bd458069
Callum Eddisford <me@callumeddisford.co.uk>
2021-03-31 12:46:27 +0100

    Fix type

--- src/Models/Field.ts
+++ src/Models/Field.ts
@@ -2,9 +2,9 @@ import Xuid, {SupportedXuid} from "../Utils/Xuid";
 
 export default interface Field extends Object {
     _id: Xuid<SupportedXuid.File>;
-    field_name: String,
-    free_text: String,
-    field_id: String,
-    type: String,
-    mapping_id: String,
+    field_name: string,
+    free_text: string,
+    field_id: string,
+    type: string,
+    mapping_id: string,
 }
